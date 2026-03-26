6e2aecc5f2bd848a5eb27b1c3c82acb3f7eb8cba
Marcus Copacheski <marcusdanielcopacheski@gmail.com>
2026-03-26 19:30:32 -0300

    Adicionada logica inicial do jogo

--- JogoAdivinhacao.ConsoleApp/Program.cs
+++ JogoAdivinhacao.ConsoleApp/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+while (true)
+{
+  Console.Clear();
+
+  Console.WriteLine("--------------------------");
+  Console.WriteLine("Jogo de Adivinhação");
+  Console.WriteLine("--------------------------");
+
+  Console.Write("Digite um número: ");
+  int num = Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine($"Numero digitado: {num}");
+
+  int numAletorio = RandomNumberGenerator.GetInt32(1, 21);
+  Console.WriteLine($"Numero aleatorio: {numAletorio}");
+
+  if (num == numAletorio)
+  {
+    Console.WriteLine("--------------------------");
+    Console.WriteLine("Parabens voce acertou");
+    Console.WriteLine("--------------------------");
+  }
+  else if (num > numAletorio)
+  {
+    Console.WriteLine("--------------------------");
+    Console.WriteLine("O número digitado foi maior do que o número secreto!");
+    Console.WriteLine("--------------------------");
+  }
+  else
+  {
+    Console.WriteLine("--------------------------");
+    Console.WriteLine("O número digitado foi menor do que o número secreto!");
+    Console.WriteLine("--------------------------");
+  }
+
+  Console.WriteLine("Deseja continuar jogando? [s/n] ");
+  string? opcaoContinuar = Console.ReadLine();
+
+  if (opcaoContinuar?.ToUpper() != "S")
+  {
+    break;
+  }
+
+  Console.ReadLine();
+
+}
